31d9b0ddae84635ed31151779ca51dba923f77e2
eve <eve.guerard90@gmail.com>
2023-01-09 07:06:28 +0100

    change class habit.ts

--- src/app/models/habit.ts
+++ src/app/models/habit.ts
@@ -1,5 +1,5 @@
 export class Habit {
-    name: string[] = [];
-    frequency: string[] = [];
-    description: string[] = [];
+    name: string;
+    frequency: string;
+    description: string;
 };

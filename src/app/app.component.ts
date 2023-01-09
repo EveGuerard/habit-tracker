b917addee0a7e230b971c2a34baf5a59a55ff62b
eve <eve.guerard90@gmail.com>
2023-01-09 07:03:12 +0100

    version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Habit } from './models/habit';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'habit-tracker';
+  public adding = false;
+  public editing = false;
+  public editingIndex: number;
+
+  public habitForm = new FormGroup({
+    name: new FormControl(''),
+    frequency: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  public habits: Habit[] = [
+    <Habit>{
+      name: 'Promenade de 15 minutes',
+      frequency: 'Chaque jour',
+      description: 'Se promener est bien pour la santé.',
+    },
+    <Habit>{
+      name: 'Arroser les fleurs',
+      frequency: 'Une fois par semaine',
+      description: 'Un peu de verdure à la maison',
+
+    },
+  ];
+  public onSubmit() {
+    const habit = this.habitForm.value as Habit;
+
+    if (this.editing) {
+      this.habits.splice(this.editingIndex, 1, habit);
+    } else {
+      this.habits.push(habit);
+    }
+
+    this.editing = false;
+    this.adding = false;
+  }
+
+  public setEditForm(habit: Habit, index: number) {
+    this.habitForm.patchValue({
+      name: habit.name,
+      frequency: habit.frequency,
+      description: habit.description,
+    });
+    this.editing = true;
+    this.editingIndex = index;
+  }
+
+  public onDelete(index: number) {
+    this.habits.splice(index, 1);
+  }
+
+  exitForm() {
+    this.adding = false;
+    this.editing = false;
+    this.habitForm.reset();
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
